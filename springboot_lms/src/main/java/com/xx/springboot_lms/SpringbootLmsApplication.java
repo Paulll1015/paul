012e8c2ecb9bb3575d318d1183f5de0bb3fcea38
阿Paull <82443922+Paulll1015@users.noreply.github.com>
2023-07-07 09:41:06 +0800

    Add files via upload

---
+++ springboot_lms/src/main/java/com/xx/springboot_lms/SpringbootLmsApplication.java
@@ -0,0 +1,13 @@
+package com.xx.springboot_lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLmsApplication.class, args);
+    }
+
+}
---
+++ springboot_lms/src/test/java/com/xx/springboot_lms/SpringbootLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xx.springboot_lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
